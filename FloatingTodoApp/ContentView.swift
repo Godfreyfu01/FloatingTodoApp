9f7cd9a963b2771e5be91e57319a96ee5b5cec00
godfrey <@>
2024-06-02 03:23:41 +0800

    Initial Commit

---
+++ FloatingTodoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FloatingTodoApp
+//
+//  Created by Godfrey on 2/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FloatingTodoApp/FloatingTodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTodoAppApp.swift
+//  FloatingTodoApp
+//
+//  Created by Godfrey on 2/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
